1a488365b4af6cfb6e71cc4eba724080c89876b2
RKiddle <RKiddle@users.noreply.github.com>
2020-01-24 21:31:47 +0700

    Create Team_Stats.js

---
+++ Team_Stats.js
@@ -0,0 +1,25 @@
+const team = {
+  _gmaes: [],
+  _players: [
+    {
+      firstName: 'Miguel',
+      lastName: 'Almirón',
+      age: 25,
+    
+    
+      firstName: 'Yoshinori',
+      lastName: 'Muto',
+      age: 27,
+    
+    
+      firstName: 'Sean',
+      lastName: 'Longstaff',  
+      age: 22,
+    
+      
+      firstName: 'Andrew',
+      lastName: 'Carroll',
+      age: 31,
+      }
+  ],
+};
